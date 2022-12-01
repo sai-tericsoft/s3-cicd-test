9af38db7293ec32e5223088f7fc26ea42fb8b46b
Faizan Khan <117180277+faizan-tericsoft@users.noreply.github.com>
2022-12-01 17:35:21 +0530

    minor changes

--- src/screens/admin/client/client-activity-log/ClientActivityLogComponent.tsx
+++ src/screens/admin/client/client-activity-log/ClientActivityLogComponent.tsx
@@ -1,10 +1,11 @@
 import "./ClientActivityLogComponent.scss";
 import TableWrapperComponent from "../../../../shared/components/table-wrapper/TableWrapperComponent";
 import {APIConfig, ENV} from "../../../../constants";
-import TableComponent from "../../../../shared/components/table/TableComponent";
-// import moment from "moment";
-import moment from 'moment-timezone';
+
+
+
 import {CommonService} from "../../../../shared/services";
+import TableComponent from "../../../../shared/components/table/TableComponent";
 
 
 interface ClientActivityLogComponentProps {
@@ -26,55 +27,56 @@ const data = [
     }
 ]
 
-const column = [
-    {
-        key: 'activity',
-        title: 'Activity',
-        width: '50%',
-        render: (item: any) => {
-            return <div>
-                <div className={'client-activity-log-section'}>
-                    {item.section}
-                </div>
-                <div className={'client-activity-log-subsection'}>
-                    {item.subSection} &gt; {item.field} :   {item.oldValue} &rarr; {item.newValue}
-                </div>
-            </div>
-        }
 
-    },
-    {
-        key: 'staff',
-        title: 'Staff',
-        width: "25%",
-        render: (item: any) => {
-            return <>{item.updateBy.first_name}&nbsp;{item.updateBy.last_name}</>
-        },
-
-    },
-    {
-        key: 'date/time',
-        title: 'Date/Time Stamp',
-        width: "25%",
-        render: (item: any) => {
-            return <>
-                {CommonService.transformTimeStamp(item.updateAt)}
-            </>
-        }
-
-    }
-]
 
 
 const ClientActivityLogComponent = (props: ClientActivityLogComponentProps) => {
 
+    const client_activity_log_column = [
+        {
+            key: 'activity',
+            title: 'Activity',
+            width: '50%',
+            render: (item: any) => {
+                return <div>
+                    <div className={'client-activity-log-section'}>
+                        {item.section}
+                    </div>
+                    <div className={'client-activity-log-subsection'}>
+                        {item.subSection} &gt; {item.field} :   {item.oldValue} &rarr; {item.newValue}
+                    </div>
+                </div>
+            }
+
+        },
+        {
+            key: 'staff',
+            title: 'Staff',
+            width: "25%",
+            render: (item: any) => {
+                return <>{item.updateBy.first_name}&nbsp;{item.updateBy.last_name}</>
+            },
+
+        },
+        {
+            key: 'date/time',
+            title: 'Date/Time Stamp',
+            width: "25%",
+            render: (item: any) => {
+                return <>
+                    {CommonService.transformTimeStamp(item.updateAt)}
+                </>
+            }
+
+        }
+    ]
     return (
         <div>
             <TableWrapperComponent
                 url={APIConfig.CLIENT_ACTIVITY_LOG.URL}
                 method={APIConfig.CLIENT_ACTIVITY_LOG.METHOD}
-                isPaginated={false} columns={column}/>
-            {/*<TableComponent data={data} columns={column}/>*/}
+                isPaginated={false} columns={client_activity_log_column}/>
+            <TableComponent data={data} columns={client_activity_log_column}/>
         </div>
     );
 
