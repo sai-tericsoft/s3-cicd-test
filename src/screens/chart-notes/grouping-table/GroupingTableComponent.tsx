4a7bfe5a7f5e7e3c9dc66aa63cfae93c08a6e15e
Manoj varma <34647667+manojvarmaboppisetti@users.noreply.github.com>
2022-12-21 16:02:40 +0530

    KIN-423 reviews grouping table spike

--- src/screens/chart-notes/grouping-table/GroupingTableComponent.tsx
+++ src/screens/chart-notes/grouping-table/GroupingTableComponent.tsx
@@ -1,27 +1,148 @@
-import {Table} from 'antd';
+import TableComponent from "../../../shared/components/table/TableComponent";
+import React from "react";
 
 interface GroupingTableComponentProps {
-    columns:any;
-    bordered?:boolean;
-    data:any;
-    className?:any;
 }
 
 const GroupingTableComponent = (props: GroupingTableComponentProps) => {
 
-    const {bordered,columns,data,className}=props;
+    const columns: any = [
+        {
+            title: 'Movement',
+            dataIndex: 'movement',
+            key: 'movement',
+            width: 150,
+            fixed: 'left',
+            align: "center"
+        },
 
-        return <>
-            <Table
-                className={className}
-                columns={columns}
-                dataSource={data}
-                bordered={bordered}
-                size={'middle'}
 
-            />
-        </>
-    }
-;
+        {
+            title: 'Left Side',
+            className: 'left_side',
+            children: [
+                {
+                    title: 'AROM',
+                    dataIndex: 'arom',
+                    key: 'arom',
+                    align: "center"
+                },
+                {
+                    title: 'PROM',
+                    dataIndex: 'prom',
+                    key: 'prom',
+                    align: "center"
+                },
+                {
+                    title: 'Strength',
+                    dataIndex: 'strength',
+                    key: 'strength',
+                    align: "center"
+                }
+            ]
+
+        },
+        {
+            title: 'Right Side',
+            children: [
+
+                {
+                    title: 'AROM',
+                    dataIndex: 'arom',
+                    key: 'arom',
+                    align: "center"
+
+
+                },
+                {
+                    title: 'PROM',
+                    dataIndex: 'prom',
+                    key: 'prom',
+                    align: "center"
+                },
+                {
+                    title: 'Strength',
+                    dataIndex: 'strength',
+                    key: 'strength',
+                    align: "center"
+                }
+            ]
+
+        },
+        {
+            title: 'Central',
+            children: [
+                {
+                    title: 'AROM',
+                    dataIndex: 'arom',
+                    key: 'arom',
+                    align: "center"
+                },
+                {
+                    title: 'PROM',
+                    dataIndex: 'prom',
+                    key: 'prom',
+                    align: "center"
+                },
+                {
+                    title: 'Strength',
+                    dataIndex: 'strength',
+                    key: 'strength',
+                    align: "center"
+                }
+            ]
+
+        },
+        {
+            title: 'Bilateral',
+            children: [
+                {
+                    title: 'AROM',
+                    dataIndex: 'arom',
+                    key: 'arom',
+                    align: "center",
+                },
+                {
+                    title: 'PROM',
+                    dataIndex: 'prom',
+                    key: 'prom',
+                    align: "center"
+                },
+                {
+                    title: 'Strength',
+                    dataIndex: 'strength',
+                    key: 'strength',
+                    align: "center"
+                }
+            ]
+
+        },
+    ];
+
+    const data: any = [
+        {
+            movement: 'Flexion',
+            arom: '130',
+            prom: '0',
+            strength: '5/5',
+        },
+        {
+            movement: 'Flexion',
+            arom: '130',
+            prom: '0',
+            strength: '5/5'
+        },
+        {
+            movement: 'Flexion',
+            arom: '130',
+            prom: '0',
+            strength: '5/5'
+        }
+    ]
+
+    return (
+        <TableComponent data={data} columns={columns} bordered={true}/>
+    );
+}
 
 export default GroupingTableComponent;
