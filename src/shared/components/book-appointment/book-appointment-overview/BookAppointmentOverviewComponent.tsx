18bdf5daf643541dd2bc8c18b3f6d38ce20c44e0
Santhosh Banda <banda.santhosh007@gmail.com>
2023-02-04 16:21:59 +0530

    book appointment and checkin flow tweeks

--- src/shared/components/book-appointment/book-appointment-overview/BookAppointmentOverviewComponent.tsx
+++ src/shared/components/book-appointment/book-appointment-overview/BookAppointmentOverviewComponent.tsx
@@ -3,10 +3,8 @@ import "./BookAppointmentOverviewComponent.scss";
 import {CommonService} from "../../../services";
 import {IAPIResponseType} from "../../../models/api.model";
 import ChipComponent from "../../chip/ChipComponent";
-import AvatarComponent from "../../avatar/AvatarComponent";
 import {ImageConfig} from "../../../../constants";
 import ButtonComponent from "../../button/ButtonComponent";
-import moment from "moment/moment";
 import {useSelector} from "react-redux";
 import {IRootReducerState} from "../../../../store/reducers";
 
@@ -22,29 +20,29 @@ const BookAppointmentOverviewComponent = (props: BookAppointmentOverviewComponen
     const {onBack, onComplete, bookingDraft} = props;
     const {appointmentTypes} = useSelector((state: IRootReducerState) => state.staticData);
     const [isBookingLoading, setIsBookingLoading] = useState<boolean>(false);
-    const [serviceDetails, setServiceDetails] = useState<any | null>(null);
+    // const [serviceDetails, setServiceDetails] = useState<any | null>(null);
     const [bookType, setBookType] = useState<any | null>(null);
 
 
-    const getServiceView = useCallback(
-        (serviceId: string) => {
-            setServiceDetails([]);
-            CommonService._service.ServiceDetailsAPICall(serviceId)
-                .then((response: IAPIResponseType<any>) => {
-                    setServiceDetails(response.data);
-                })
-                .catch((error: any) => {
-                    setServiceDetails(null);
-                });
-        },
-        [],
-    );
+    // const getServiceView = useCallback(
+    //     (serviceId: string) => {
+    //         setServiceDetails([]);
+    //         CommonService._service.ServiceDetailsAPICall(serviceId)
+    //             .then((response: IAPIResponseType<any>) => {
+    //                 setServiceDetails(response.data);
+    //             })
+    //             .catch((error: any) => {
+    //                 setServiceDetails(null);
+    //             });
+    //     },
+    //     [],
+    // );
 
-    useEffect(() => {
-        if (bookingDraft && bookingDraft.service) {
-            getServiceView(bookingDraft.service._id);
-        }
-    }, [getServiceView, bookingDraft]);
+    // useEffect(() => {
+    //     if (bookingDraft && bookingDraft.service) {
+    //         getServiceView(bookingDraft.service._id);
+    //     }
+    // }, [getServiceView, bookingDraft]);
 
     useEffect(() => {
         const type = appointmentTypes?.find(v => v.code === bookingDraft.appointment_type);
@@ -62,8 +60,9 @@ const BookAppointmentOverviewComponent = (props: BookAppointmentOverviewComponen
                 service_id: booking.service._id,
                 provider_id: booking.provider.provider_id,
                 appointment_type: booking.appointment_type,
+                consultation_title: booking.duration.consultation_title,
                 appointment_date: booking.date,
-                duration: booking.duration,
+                duration: parseInt(booking.duration.duration),
                 start_time: booking.time.start_min,
                 end_time: booking.time.end_min,
 
@@ -100,72 +99,71 @@ const BookAppointmentOverviewComponent = (props: BookAppointmentOverviewComponen
                 {/*         }><ImageConfig.CloseIcon/></div>*/}
                 {/*</ToolTipComponent>*/}
             </div>
+            <div className="details-header">
+                <div className="block-body pdd-top-20">
+                    <div className="block-content">
+                        <ImageConfig.CalendarIcon/>
+                        <div
+                            className="content-title">{CommonService.convertDateFormat(bookingDraft.appointment_date)}</div>
+                    </div>
+                    <div className="block-content">
+                        <ImageConfig.TimerIcon/>
+                        <div
+                            className="content-title">{CommonService.getHoursAndMinutesFromMinutes(bookingDraft.time.start_min)}</div>
+                    </div>
+                    <div className="block-content">
+                    </div>
+                    <div className="block-content">
+                    </div>
+                </div>
+            </div>
             <div className="flex-1">
-                <div className="mrg-bottom-20">
-                    <div className="d-inline">
-                        <ChipComponent color={'success'} label={bookType?.title}/>&nbsp;
-                        <ChipComponent color={'success'} label={bookingDraft.duration + ' min'}/>
-                    </div>
-                </div>
-                <div className="overview-block overview-services-block">
-                    <div className="block-body">
-                        <div className="avatar-wrapper">
-                            {serviceDetails && <AvatarComponent type={'circle'} url={serviceDetails?.image?.url}/>}
-                            {!serviceDetails && <AvatarComponent type={'circle'}/>}
+                <div className="details-box">
+                    <div className="details-header">Details</div>
+                    <div className="details-body">
+                        <div className="details-body-block">
+                            <div className="details-body-item">
+                                <div className="item-heading"><ImageConfig.PersonIcon/>&nbsp;Client Name</div>
+                                <div
+                                    className="item-value">{bookingDraft?.client?.first_name + ' ' + bookingDraft?.client?.last_name}</div>
+                            </div>
+                            <div className="details-body-item">
+                                <div className="item-heading"><ImageConfig.CallIcon/>&nbsp;Phone Number</div>
+                                <div
+                                    className="item-value">{bookingDraft?.client?.primary_contact_info?.phone ? CommonService.formatPhoneNumber(bookingDraft?.client?.primary_contact_info?.phone) : 'N/A'}</div>
+                            </div>
+                            <div className="details-body-item">
+                                <div className="item-heading"><ImageConfig.EmailIcon/>&nbsp;Email</div>
+                                <div
+                                    className="item-value">{bookingDraft?.client?.primary_email || 'N/A'}</div>
+                            </div>
                         </div>
-                        <div className="block-content">
-                            <div className="service-category-name">{bookingDraft.service_category.name}</div>
-                            <div className="service-name">{bookingDraft.service.name}</div>
+                        <div className="details-body-block">
+                            <div className="details-body-item">
+                                <div className="item-heading"><ImageConfig.AssignmentIcon/>&nbsp;Provider Name
+                                </div>
+                                <div
+                                    className="item-value">
+                                    <div className="mrg-bottom-10">
+                                        {bookingDraft?.provider?.provider_name}
+                                    </div>
+                                    <div className="d-inline">
+                                        <ChipComponent color={'success'} label={bookType?.title}/>&nbsp;
+                                        <ChipComponent color={'success'}
+                                                       label={bookingDraft?.duration.duration + ' min'}/>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                        <div className="details-body-block">
+                            <div className="details-body-item">
+                                <div className="item-heading"><ImageConfig.MedicalServicesIcon/>&nbsp;Service
+                                </div>
+                                <div
+                                    className="item-value">{bookingDraft?.service_category?.name || 'N/A'} / {bookingDraft?.service?.name || 'N/A'}</div>
+                            </div>
+                        </div>
 
-                <div className="overview-block">
-                    <div className="block-heading">
-                        Client Details
-                    </div>
-                    <div className="block-body">
-                        <div className="avatar-wrapper">
-                            <AvatarComponent className={'blue-avatar'}
-                                             title={bookingDraft.client.first_name + ' ' + bookingDraft.client.last_name}/>
-                        </div>
-                        <div className="block-content">
-                            <div
-                                className="content-title">{bookingDraft.client.first_name + ' ' + bookingDraft.client.last_name}</div>
-                            <div
-                                className="content-sub-title">{bookingDraft.client?.primary_contact_info?.phone && CommonService.formatPhoneNumber(bookingDraft.client?.primary_contact_info?.phone)}</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="overview-block">
-                    <div className="block-heading">
-                        Provider:
-                    </div>
-                    <div className="block-body">
-                        <div className="avatar-wrapper">
-                            <AvatarComponent className={'green-avatar'} title={bookingDraft.provider.provider_name}/>
-                        </div>
-                        <div className="block-content">
-                            <div className="content-title">{bookingDraft.provider.provider_name}</div>
-                            <div className="content-sub-title">{bookingDraft.provider.role || '-'}</div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className={'overview-block overview-block-time green-card'}>
-                    <div className="block-heading">
-                        Date and Time:
-                    </div>
-                    <div className="block-body ts-row pdd-top-20">
-                        <div className="ts-col block-content">
-                            <ImageConfig.CalendarIcon/>
-                            <div className="content-title">{CommonService.convertDateFormat(bookingDraft.date)}</div>
-                        </div>
-                        <div className="ts-col block-content">
-                            <ImageConfig.TimerIcon/>
-                            <div
-                                className="content-title">{moment(bookingDraft.time.start, 'hh:mm').format('hh:mm A')}</div>
-                        </div>
                     </div>
                 </div>
             </div>
