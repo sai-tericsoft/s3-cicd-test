294e28b31d4dacd3584aee3c60b92b0f6e14795f
Faizan Khan <117180277+faizan-tericsoft@users.noreply.github.com>
2023-03-02 12:06:32 +0530

    KIN-858 updates css

--- src/screens/chart-notes/add-medical-record-document/AddMedicalRecordDocumentComponent.tsx
+++ src/screens/chart-notes/add-medical-record-document/AddMedicalRecordDocumentComponent.tsx
@@ -189,7 +189,7 @@ const AddMedicalRecordDocumentComponent = (props: AddMedicalRecordDocumentCompon
                                         </>
                                     </div>
                                 </div>
-                                <div className="t-form-actions">
+                                <div className="t-form-actions mrg-top-40 mrg-bottom-30">
                                     <ButtonComponent className={'mrg-right-15'}
                                         variant={"outlined"}
                                         id={"medical_intervention_add_cancel_btn"}
