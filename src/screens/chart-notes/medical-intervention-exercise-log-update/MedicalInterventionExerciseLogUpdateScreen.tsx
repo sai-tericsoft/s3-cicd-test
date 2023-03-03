5404278d19f47f1270593a625e2d793a048a63fb
Faizan Khan <117180277+faizan-tericsoft@users.noreply.github.com>
2023-03-03 12:45:26 +0530

    KIN-1132 fixes alignment issue

--- src/screens/chart-notes/medical-intervention-exercise-log-update/MedicalInterventionExerciseLogUpdateScreen.tsx
+++ src/screens/chart-notes/medical-intervention-exercise-log-update/MedicalInterventionExerciseLogUpdateScreen.tsx
@@ -92,6 +92,7 @@ const MedicalInterventionExerciseLogUpdateScreen = (props: MedicalInterventionEx
             title: 'Exercise Name',
             key: 'name',
             width: 280,
+            align:'center',
             render: (record: any, index: any) => {
                 return <Field
                     name={`exercise_records.${index}.name`}
@@ -111,6 +112,7 @@ const MedicalInterventionExerciseLogUpdateScreen = (props: MedicalInterventionEx
             title: 'SET',
             key: 'set',
             width: 150,
+            align: 'center',
             render: (record: any, index: any) => {
                 return <Field
                     name={`exercise_records.${index}.no_of_sets`}
@@ -130,6 +132,7 @@ const MedicalInterventionExerciseLogUpdateScreen = (props: MedicalInterventionEx
             title: 'REP',
             key: 'rep',
             width: 150,
+            align: 'center',
             render: (record: any, index: any) => {
                 return <Field
                     name={`exercise_records.${index}.no_of_reps`}
@@ -149,6 +152,7 @@ const MedicalInterventionExerciseLogUpdateScreen = (props: MedicalInterventionEx
             title: 'TIME',
             key: 'time',
             width: 150,
+            align: 'center',
             render: (record: any, index: any) => {
                 return <Field
                     name={`exercise_records.${index}.time`}
@@ -168,6 +172,7 @@ const MedicalInterventionExerciseLogUpdateScreen = (props: MedicalInterventionEx
             title: 'RESISTANCE',
             key: 'resistance',
             width: 150,
+            align: 'center',
             render: (record: any, index: any) => {
                 return <Field
                     name={`exercise_records.${index}.resistance`}
@@ -186,6 +191,7 @@ const MedicalInterventionExerciseLogUpdateScreen = (props: MedicalInterventionEx
         {
             title: '',
             key: 'actions',
+            align: 'center',
             render: (record: any, index: any) => {
                 return (
                     <>
