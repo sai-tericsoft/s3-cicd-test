5d5739401c47f3ed0fc8626df120402351895574
divyamandapati <91051550+divyamandapati@users.noreply.github.com>
2023-03-17 21:26:00 +0530

    #1367 spacing issue between state and zipcode fixed

--- src/screens/billings/billing-details/BillingDetailsScreen.tsx
+++ src/screens/billings/billing-details/BillingDetailsScreen.tsx
@@ -419,7 +419,8 @@ const BillingDetailsScreen = (props: BillingDetailsScreenProps) => {
                                             <div
                                                 className={"billing-address-block__detail__row"}> {billingDetails?.billing_address.address_line} </div>
                                             <div className={"billing-address-block__detail__row"}>
-                                                <span>{billingDetails?.billing_address?.city}</span>, <span>{billingDetails?.billing_address?.state}</span>
+                                                <span>{billingDetails?.billing_address?.city}</span>,
+                                                <span>{billingDetails?.billing_address?.state}</span>&nbsp;
                                                 <span>{billingDetails?.billing_address?.zip_code}</span>
                                             </div>
                                             <div
