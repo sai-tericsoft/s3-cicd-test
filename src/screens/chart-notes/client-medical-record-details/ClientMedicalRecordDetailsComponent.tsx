163a18e9326ad5cfa03c7f58b34b73c066912a63
divyamandapati <91051550+divyamandapati@users.noreply.github.com>
2023-04-16 23:07:19 +0530

    repeat medical intervention bug fixs

--- src/screens/chart-notes/client-medical-record-details/ClientMedicalRecordDetailsComponent.tsx
+++ src/screens/chart-notes/client-medical-record-details/ClientMedicalRecordDetailsComponent.tsx
@@ -123,7 +123,7 @@ const ClientMedicalRecordDetailsComponent = (props: ClientMedicalDetailsComponen
                 CommonService._alert.showToast(response[Misc.API_RESPONSE_MESSAGE_KEY], "success");
                 navigate(CommonService._routeConfig.UpdateMedicalIntervention(medicalRecordId, response?.data._id) + '?showClear=true');
                 setIsMedicalInterventionBeingRepeated(false);
-                selectedAppointment(null)
+                setSelectedAppointment(null)
             }).catch((error: any) => {
                 CommonService._alert.showToast(error?.error || "Error repeating last medical intervention", "error");
                 setIsMedicalInterventionBeingRepeated(false);
