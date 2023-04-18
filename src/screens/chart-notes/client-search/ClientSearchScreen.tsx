a6835d14074fbd3ea155df6f0f5acc2a6def617a
Faizan Khan <117180277+faizan-tericsoft@users.noreply.github.com>
2023-04-18 17:21:58 +0530

    KIN-1683 adds comma between last and first name

--- src/screens/chart-notes/client-search/ClientSearchScreen.tsx
+++ src/screens/chart-notes/client-search/ClientSearchScreen.tsx
@@ -48,7 +48,7 @@ const ClientSearchScreen = (props: ClientSearchScreenProps) => {
             align:'center',
             width: 250,
             render: (item: IClientBasicDetails) => {
-                return <span>{item?.last_name} {item?.first_name}</span>
+                return <span>{item?.last_name}{","}{item?.first_name}</span>
             }
         },
         {
