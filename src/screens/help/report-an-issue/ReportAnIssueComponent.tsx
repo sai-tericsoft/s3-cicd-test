aea23cd26b270b05e58b10e29aff4815739f3c4e
Faizan Khan <117180277+faizan-tericsoft@users.noreply.github.com>
2023-04-25 16:14:35 +0530

    updates character limit number

--- src/screens/help/report-an-issue/ReportAnIssueComponent.tsx
+++ src/screens/help/report-an-issue/ReportAnIssueComponent.tsx
@@ -24,7 +24,7 @@ const ReportAnIssueInitialValues: any = {
 }
 
 const ReportAnIssueValidationSchema = Yup.object().shape({
-    issue: Yup.string().max(500, ''),
+    issue: Yup.string().max(1000, ' '),
 });
 
 const ReportAnIssueComponent = (props: ReportAnIssueComponentProps) => {
@@ -83,11 +83,11 @@ const ReportAnIssueComponent = (props: ReportAnIssueComponentProps) => {
 
                                     </div>
                                     <div className={'ts-col-md-12'}>
-                                        {(values.issue.length) >= 500 ?
+                                        {(values.issue.length) >= 1000 ?
                                         <div className={'alert-error'}> Characters Limit
-                                            : {(values.issue.length)}/500</div> :
+                                            : {(values.issue.length)}/1000</div> :
                                         <div className={'no-alert'}> Characters Limit
-                                            : {(values.issue.length)}/500</div>}
+                                            : {(values.issue.length)}/1000</div>}
                                     </div>
                                     <div className={'ts-col-md-12'}>
                                         <FormControlLabelComponent label={"Attachment (if any)"}/>
@@ -130,8 +130,7 @@ const ReportAnIssueComponent = (props: ReportAnIssueComponentProps) => {
                                 <div className="t-form-actions">
                                     <ButtonComponent type={'submit'}
                                                      isLoading={isIssueSubmitting}
-
-                                                     disabled={!isValid || isIssueSubmitting}>
+                                                     disabled={!isValid || isIssueSubmitting ||( values.issue?.length===0 && values.attachment?.length===0) }>
                                         Submit
                                     </ButtonComponent>
                                 </div>
