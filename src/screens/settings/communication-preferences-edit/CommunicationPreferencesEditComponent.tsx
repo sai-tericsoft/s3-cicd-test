7b95f189953a831f8a11b0a39bd3861a0968a760
Faizan Khan <117180277+faizan-tericsoft@users.noreply.github.com>
2023-07-01 21:18:48 +0530

    KIN-2142 fixes-view-provider-details-bug

--- src/screens/settings/communication-preferences-edit/CommunicationPreferencesEditComponent.tsx
+++ src/screens/settings/communication-preferences-edit/CommunicationPreferencesEditComponent.tsx
@@ -94,7 +94,7 @@ const CommunicationPreferencesEditComponent = (props: CommunicationPreferencesCo
                         }, [validateForm, values]);
                         return (
                             <Form noValidate={true} className={"t-form"}>
-                                <FormDebuggerComponent showDebugger={true} values={values} errors={errors}/>
+                                {/*<FormDebuggerComponent showDebugger={true} values={values} errors={errors}/>*/}
                                 <div className={'ts-row'}>
                                     <div className={'ts-col-6'}>
                                         <QuestionComponent title={'Appointment Reminders'}
