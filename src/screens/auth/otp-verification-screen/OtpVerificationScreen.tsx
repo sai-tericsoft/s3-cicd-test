d856445d855ae90a773b61125087f481704cadf4
im-Debabrata <debabrata@tericsoft.com>
2023-09-30 14:48:53 +0530

    otp timer | Link Component
    - Implemented OTP timer functionality
    - Make changes in Link component as per need

--- src/screens/auth/otp-verification-screen/OtpVerificationScreen.tsx
+++ src/screens/auth/otp-verification-screen/OtpVerificationScreen.tsx
@@ -1,13 +1,9 @@
 import "./OtpVerificationScreen.scss";
 import {Field, FieldProps, Form, Formik, FormikHelpers} from "formik";
 import {useCallback, useEffect, useState} from "react";
-import FormikInputComponent from "../../../shared/components/form-controls/formik-input/FormikInputComponent";
 import ButtonComponent from "../../../shared/components/button/ButtonComponent";
 import LinkComponent from "../../../shared/components/link/LinkComponent";
-import {LOGIN_ROUTE, OTP_VERIFICATION_ROUTE, RESET_PASSWORD_ROUTE} from "../../../constants/RoutesConfig";
 import FormikOTPComponent from "../../../shared/components/form-controls/formik-otp/FormikOtpComponent";
-import {useDispatch} from "react-redux";
-import {useNavigate, useLocation} from "react-router-dom";
 import {CommonService} from "../../../shared/services";
 import * as Yup from "yup";
 import commonService from "../../../shared/services/common.service";
@@ -28,9 +24,42 @@ const otpFormInitialValues = {
     otp: ""
 }
 
+const OTP_VALIDITY = 60;
+
 const OtpVerificationScreen = (props: OtpVerificationScreenProps) => {
     const [isLoading, setIsLoading] = useState(false);
+    const [otpTimer, setOtpTimer] = useState(0);
+    const [isOTPBeingRequested, setIsOTPBeingRequested] = useState<boolean>(false);
     const handleNavigation = useHandleNavigation();
+
+    const runOTPCountdown = useCallback(() => {
+        let timer = OTP_VALIDITY;
+        setOtpTimer(timer);
+        const interval = setInterval(() => {
+            timer--;
+            setOtpTimer(timer);
+            if (timer === 0) {
+                clearInterval(interval);
+            }
+        }, 1000);
+    }, []);
+
+    const handleResendOTP = useCallback((values: any) => {
+        setIsOTPBeingRequested(true);
+        CommonService._account.SendVerificationOtp(values)
+            .then((response: any) => {
+                // CommonService._alert.showToast(response[Misc.API_RESPONSE_MESSAGE_KEY], "success");
+                runOTPCountdown();
+                setIsOTPBeingRequested(false);
+            })
+            .catch((error: any) => {
+                CommonService._alert.showToast(error.error || 'Error sending OTP', "error");
+                setIsOTPBeingRequested(false);
+            }).finally(() => {
+            runOTPCountdown();
+            setIsOTPBeingRequested(false);
+        });
+    }, [runOTPCountdown]);
 
     const onSubmit = useCallback((values: any, {setSubmitting, setErrors}: FormikHelpers<any>) => {
         setIsLoading(true);
@@ -89,7 +118,23 @@ const OtpVerificationScreen = (props: OtpVerificationScreenProps) => {
                                     </Field>
                                     <div className="form-option otp-option">
                                         <div className="error-message">Entered wrong code please try again</div>
-                                        <div className="resend-otp">Resend</div>
+                                        {
+                                            otpTimer > 0 ?
+                                                <LinkComponent
+                                                    disabled={true}
+                                                    className={'otp-info text-decoration-none'}>
+                                                    {"Resend OTP in "}
+                                                    {CommonService.getMinsAndSecondsFromSeconds(otpTimer).minutes + " : " + CommonService.getMinsAndSecondsFromSeconds(otpTimer).seconds}
+                                                </LinkComponent>
+                                                :
+                                                <LinkComponent
+                                                    onClick={() => handleResendOTP(values)}
+                                                    disabled={isOTPBeingRequested}
+                                                    className={'otp-info'}
+                                                >
+                                                    {isOTPBeingRequested ? "Sending OTP" : "Resend OTP"}
+                                                </LinkComponent>
+                                        }
                                     </div>
                                 </div>
                                 <div className="t-form-actions">
