3be17063fa2c7f09a473a093baca09f3526a6d61
saicharan-tericsoft <105701025+saicharan-tericsoft@users.noreply.github.com>
2023-10-03 15:48:42 +0530

    KIN-2999-icd-11-tool-asthetics-matching

--- src/shared/components/icd-coding-tool/IcdCodingToolComponent.tsx
+++ src/shared/components/icd-coding-tool/IcdCodingToolComponent.tsx
@@ -2,6 +2,7 @@ import "./IcdCodingToolComponent.scss";
 import {useEffect, useState} from "react";
 import * as ECT from "@whoicd/icd11ect";
 import "@whoicd/icd11ect/style.css";
+
 interface IcdCodingToolComponentProps {
 
 }
@@ -17,7 +18,35 @@ const IcdCodingToolComponent = (props: IcdCodingToolComponentProps) => {
         };
 
         const callbacks = {
-            selectedEntityFunction: (selectedEntity:any) => {
+            selectedEntityFunction: (selectedEntity: any) => {
+                // bestMatchText
+                //     :
+                //     "Muscle dysmorphia"
+                // code
+                //     :
+                //     "6B21.Z"
+                // foundationUri
+                //     :
+                //     "http://id.who.int/icd/entity/178847963"
+                // iNo
+                //     :
+                //     1
+                // linearizationUri
+                //     :
+                //     "http://id.who.int/icd/release/11/2023-01/mms/731724655/unspecified"
+                // searchQuery
+                //     :
+                //     "muscle"
+                // selectedText
+                //     :
+                //     "Muscle dysmorphia"
+                // title
+                //     :
+                //     "Body dysmorphic disorder, unspecified"
+                // uri
+                //     :
+                //     "http://id.who.int/icd/release/11/2023-01/mms/731724655/unspecified"
+                // console.log(selectedEntity);
                 alert(`ICD-11 code selected: ${selectedEntity.code}`);
                 ECT.Handler.clear(iNo);
             }
@@ -29,19 +58,21 @@ const IcdCodingToolComponent = (props: IcdCodingToolComponentProps) => {
     }, []); // empty deps array to run only on mount
 
     return (
-        <div>
-            <h1>[React] Embedded Coding Tool v1.6</h1>
-
-            Type for starting search:
-
-            <input
-                type="text"
-                className="ctw-input"
-                autoComplete="off"
-                data-ctw-ino={iNo}
-            />
-
-            <div className="ctw-window" data-ctw-ino={iNo}></div>
+        <div className={'icd-coding-tool-component'}>
+            <div className={'icd-coding-tool-title'}>ICD-11 Coding Tool v1.6</div>
+            <div className={'icd-coding-tool-content-window'}>
+                <div className={'icd-coding-search-component'}>
+                    <input
+                        type="text"
+                        className="ctw-input"
+                        autoComplete="off"
+                        placeholder={'Type for starting the search'}
+                        data-ctw-ino={iNo}
+                    />
+                    <span className="clear" onClick={ECT.Handler.clear('1')}>❌ </span>
+                </div>
+                <div className="ctw-window" data-ctw-ino={iNo}></div>
+            </div>
         </div>
     );
 
