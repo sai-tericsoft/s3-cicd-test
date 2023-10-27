736b11b0a2668f08581bf0e5920ddbcf2fc4ff89
Faizan Khan <117180277+faizan-tericsoft@users.noreply.github.com>
2023-10-27 12:43:30 +0530

    KIN-3257 updates password validation component

--- src/shared/components/password-validation/PasswordValidationComponent.tsx
+++ src/shared/components/password-validation/PasswordValidationComponent.tsx
@@ -66,23 +66,23 @@ const PasswordValidationComponent = (props: PasswordValidationComponentProps) =>
             <div className="validator-rule-list">
                 <div className={`validator-rule-item ${isLengthValid ? "valid" : 'invalid'}`}>
                     <div className="validator-rule-icon"> {isLengthValid ? <CircleCheck/> : <CircleInfo/>}</div>
-                    <div className="validator-rule-text"> A minimum 8 characters long and a maximum of 16.</div>
+                    <div className="validator-rule-text"> A minimum 8 characters long and a maximum of 16</div>
                 </div>
                 <div className={`validator-rule-item ${isDigitValid ? "valid" : 'invalid'}`}>
                     <div className="validator-rule-icon"> {isDigitValid ? <CircleCheck/> : <CircleInfo/>}</div>
-                    <div className="validator-rule-text"> At least 1 digit.</div>
+                    <div className="validator-rule-text"> At least 1 digit</div>
                 </div>
                 <div className={`validator-rule-item ${isUpperCaseValid ? "valid" : 'invalid'}`}>
                     <div className="validator-rule-icon"> {isUpperCaseValid ? <CircleCheck/> : <CircleInfo/>}</div>
-                    <div className="validator-rule-text"> At least 1 uppercase letter.</div>
+                    <div className="validator-rule-text"> At least 1 uppercase letter</div>
                 </div>
                 <div className={`validator-rule-item ${isLowerCaseValid ? "valid" : 'invalid'}`}>
                     <div className="validator-rule-icon"> {isLowerCaseValid ? <CircleCheck/> : <CircleInfo/>}</div>
-                    <div className="validator-rule-text"> At least 1 lowercase letter.</div>
+                    <div className="validator-rule-text"> At least 1 lowercase letter</div>
                 </div>
                 <div className={`validator-rule-item ${isSpecialCharacterValid ? "valid" : 'invalid'}`}>
                     <div className="validator-rule-icon"> {isSpecialCharacterValid ? <CircleCheck/> : <CircleInfo/>}</div>
-                    <div className="validator-rule-text"> At least 1 special character ( #-_!@ ).</div>
+                    <div className="validator-rule-text"> At least 1 special character ( #-_!@ )</div>
                 </div>
             </div>
         </div>
