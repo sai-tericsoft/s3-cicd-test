f9d3a119c167b4796fa45cfefb1ed037218ca27a
Faizan Khan <117180277+faizan-tericsoft@users.noreply.github.com>
2023-11-02 12:42:50 +0530

    KIN-3317 fixes age issue

--- src/screens/clients/client-basic-details-card/ClientBasicDetailsCardComponent.tsx
+++ src/screens/clients/client-basic-details-card/ClientBasicDetailsCardComponent.tsx
@@ -46,7 +46,7 @@ const ClientBasicDetailsCardComponent = (props: ClientBasicDetailsCardComponentP
                             {clientBasicDetails?.client_id}
                         </DataLabelValueComponent>
                         <DataLabelValueComponent label={'Age'} className={'age-container'}>
-                            {CommonService.getTheDifferenceBetweenDates(clientBasicDetails?.dob) || "N/A"}
+                            {clientBasicDetails?.dob ? CommonService.getTheDifferenceBetweenDates(clientBasicDetails?.dob) : 'N/A'}
                         </DataLabelValueComponent>
                     </div>
                     {
