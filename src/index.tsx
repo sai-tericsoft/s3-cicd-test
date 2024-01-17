38f99eafe78ac38ff132b598fe12807abc464b75
Santhosh Banda <banda.santhosh007@gmail.com>
2024-01-17 14:46:36 +0530

    after review changes and fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./store";
+import {createRoot} from "react-dom/client";
 
-ReactDOM.render(
+const domNode = document.getElementById('root') as HTMLElement;
+if (!domNode) throw new Error("Root node not found");
+const root = createRoot(domNode);
+root.render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter basename={process.env.PUBLIC_URL || ''}>
                 <App/>
             </BrowserRouter>
         </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
